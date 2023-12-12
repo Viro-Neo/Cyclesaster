f6ec99ca835302cd63687f1f2264ea85845ac95c
uvillanueva <91880049+uvillanueva@users.noreply.github.com>
2023-12-12 16:23:12 +0100

    [DELETE] unused structs

--- go-back/models/models.go
+++ go-back/models/models.go
@@ -1,83 +1,12 @@
 package models
 
 type DataFilters struct {
-	Id          int
-	Day         string
-	Month       string
-	Year        string
-	Birth_year  string
-	Departement string
-	Gender      string
-	Road        string
-}
-
-type Accident struct {
-	accident_id int
-	jour        string
-	mois        string
-	an          string
-	hrmn        string
-	lum         string
-	dep         string
-	com         string
-	agg         string
-	intt        string
-	atm         string
-	col         string
-	adr         string
-	lat         string
-	long        string
-}
-
-type User struct {
-	num_acc     int
-	id_vehicule int
-	num_veh     int
-	place       int
-	catu        int
-	grav        int
-	sexe        int
-	an_nais     int
-	trajet      int
-	secu        int
-	secu1       int
-	secu2       int
-	secu3       int
-	locp        int
-	actp        int
-	etatp       int
-}
-
-type Vehicle struct {
-	num_acc     int
-	id_vehicule int
-	num_veh     int
-	senc        int
-	catv        int
-	obs         int
-	obsm        int
-	choc        int
-	manv        int
-	motor       int
-	occutc      int
-}
-
-type Place struct {
-	num_acc int
-	catr    int
-	voie    int
-	v1      int
-	v2      int
-	circ    int
-	nbv     int
-	prof    int
-	pr      int
-	pr1     int
-	plan    int
-	lartpc  int
-	larrout int
-	surf    int
-	infra   int
-	situ    int
-	vma     int
+	Id         int
+	Day        string
+	Month      string
+	Year       string
+	Birth_year string
+	Department string
+	Gender     string
+	Road       string
 }
