ab1ab63557cc65ec9c21bf0d39c9e8f9a6ec4a4f
uvillanueva <91880049+uvillanueva@users.noreply.github.com>
2023-12-12 14:32:46 +0100

    [ADD] db fetch function for possible values of filters

--- go-back/database/database.go
+++ go-back/database/database.go
@@ -36,8 +36,8 @@ func FetchDataFromDB(db *sql.DB, filter1Name, filter1Value string) ([]models.Dat
 	fmt.Println("Fetching data from DB")
 	var accidents []models.DataFilters
 
-	query := fmt.Sprintf("SELECT a.num_acc, a.dep, u.an_nais FROM characteristics a "+
-		"JOIN users u ON a.num_acc = u.num_acc "+
+	query := fmt.Sprintf("SELECT c.accident_id, c.department, u.birth_year FROM characteristics c "+
+		"JOIN users u ON c.accident_id = u.accident_id "+
 		"WHERE a.%s = $1", filter1Name)
 
 	rows, err := db.Query(query, filter1Value)
@@ -58,4 +58,43 @@ func FetchDataFromDB(db *sql.DB, filter1Name, filter1Value string) ([]models.Dat
 
 	return accidents, nil
 
+}
+
+func FetchFilterValuesFromDB(db *sql.DB, filterName string) ([]string, error) {
+	tables := []string{"characteristics", "area", "users", "vehicles"}
+
+	var filterValues []string
+
+	for _, table := range tables {
+		query := fmt.Sprintf("SELECT DISTINCT %s FROM %s", filterName, table)
+
+		rows, err := db.Query(query)
+
+		if err != nil {
+			continue
+		}
+
+		for rows.Next() {
+			var value sql.NullString // Change this line
+			err := rows.Scan(&value)
+			if err != nil {
+				return nil, err
+			}
+			if value.Valid && !containsString(filterValues, value.String) { // Check if the value is valid before using it
+				filterValues = append(filterValues, value.String)
+			}
+		}
+		rows.Close()
+	}
+
+	return filterValues, nil
+}
+
+func containsString(slice []string, target string) bool {
+	for _, s := range slice {
+		if s == target {
+			return true
+		}
+	}
+	return false
 }
