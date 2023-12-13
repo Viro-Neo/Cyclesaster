1bc30a171098b5fb03cb080b92982d95186f7bc3
uvillanueva <91880049+uvillanueva@users.noreply.github.com>
2023-12-13 13:30:41 +0100

    [UPDATE] added new filters to filter struct

--- go-back/models/models.go
+++ go-back/models/models.go
@@ -1,12 +1,14 @@
 package models
 
 type DataFilters struct {
-	Id         int
-	Day        string
-	Month      string
-	Year       string
-	Birth_year string
-	Department string
-	Gender     string
-	Road       string
+	Id             int
+	Day            string
+	Month          string
+	Year           string
+	Birth_year     string
+	Department     string
+	Gender         string
+	Surface        string
+	Infrastructure string
+	Trafic         string
 }
